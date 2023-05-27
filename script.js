c0df565d266ebd3096993ece992a78c0375e29e7
zimmer <103544083+nicolezimmer@users.noreply.github.com>
2023-05-27 19:56:04 -0300

    Charly-Encripta v1.0.2

--- script.js
+++ script.js
@@ -1,128 +1,116 @@
-function encriptarEntero(num) {
-    // Convertir el número en una cadena de texto
-    let strNum = num.toString();
-  
-    // Obtener los dígitos individuales
-    let digito1 = parseInt(strNum[0]);
-    let digito2 = parseInt(strNum[1]);
-    let digito3 = parseInt(strNum[2]);
-    let digito4 = parseInt(strNum[3]);
-  
-    // Encriptar los dígitos
-    digito1 = (digito1 + 7) % 10;
-    digito2 = (digito2 + 7) % 10;
-    digito3 = (digito3 + 7) % 10;
-    digito4 = (digito4 + 7) % 10;
-  
-    // Intercambiar los dígitos
-    let temp = digito1;
-    digito1 = digito3;
-    digito3 = temp;
-  
-    temp = digito2;
-    digito2 = digito4;
-    digito4 = temp;
-  
-    // Componer el número encriptado y retornarlo
-    let encriptado = parseInt(digito1.toString() + digito2.toString() + digito3.toString() + digito4.toString());
-    return encriptado;
-  }
-  function desencriptarEntero(num) {
-    // Convertir el número en una cadena de texto
-    let strNum = num.toString();
-  
-    // Obtener los dígitos individuales
-    let digito1 = parseInt(strNum[0]);
-    let digito2 = parseInt(strNum[1]);
-    let digito3 = parseInt(strNum[2]);
-    let digito4 = parseInt(strNum[3]);
-  
-    // Intercambiar los dígitos de nuevo al orden original
-    let temp = digito1;
-    digito1 = digito3;
-    digito3 = temp;
-  
-    temp = digito2;
-    digito2 = digito4;
-    digito4 = temp;
-  
-    // Desencriptar los dígitos
-    digito1 = (digito1 + 3) % 10;
-    digito2 = (digito2 + 3) % 10;
-    digito3 = (digito3 + 3) % 10;
-    digito4 = (digito4 + 3) % 10;
-  
-    // Componer el número desencriptado y retornarlo
-    let desencriptado = parseInt(digito1.toString() + digito2.toString() + digito3.toString() + digito4.toString());
-    return desencriptado;
-  }
-  function esNumeroCuatroDigitos(variable) {
-    if (typeof variable === 'number' && Number.isInteger(variable) && variable >= 1000 && variable <= 9999) {
-      return true;
-    } else {
-      return false;
-    }
-  }
-  function copiarTextoAlPortapapeles(texto) {
-    const elementoTemporal = document.createElement('textarea');
-    elementoTemporal.value = texto;
-    document.body.appendChild(elementoTemporal);
-    elementoTemporal.select();
-    document.execCommand('copy');
-    document.body.removeChild(elementoTemporal);
+function encriptarNumero(numero) {
+  // Convertir el número en una cadena de texto y asegurar que tenga 4 dígitos
+  let strNumero = numero.toString().padStart(4, '0');
+
+  // Obtener los dígitos individuales
+  let digito1 = parseInt(strNumero[0]);
+  let digito2 = parseInt(strNumero[1]);
+  let digito3 = parseInt(strNumero[2]);
+  let digito4 = parseInt(strNumero[3]);
+
+  // Encriptar los dígitos
+  digito1 = (digito1 + 7) % 10;
+  digito2 = (digito2 + 7) % 10;
+  digito3 = (digito3 + 7) % 10;
+  digito4 = (digito4 + 7) % 10;
+
+  // Intercambiar los dígitos
+  [digito1, digito3] = [digito3, digito1];
+  [digito2, digito4] = [digito4, digito2];
+
+  // Componer el número encriptado y retornarlo
+  let numeroEncriptado = parseInt(digito1.toString() + digito2.toString() + digito3.toString() + digito4.toString());
+  return numeroEncriptado;
 }
 
-    // Obtener referencia al campo de entrada
-    const numeroInput1 = document.getElementById('numeroInput1');
-    const numeroInput2 = document.getElementById('numeroInput2');
-    const contenido1 = document.getElementById('contenido1');
-    const copiarBoton1 = document.getElementById('copiarBoton1')
-    const contenido2 = document.getElementById('contenido2');
-    const copiarBoton2 = document.getElementById('copiarBoton2')
-    // Agregar un evento de escucha al campo de entrada1
-    numeroInput1.addEventListener('input', function() {
-      // Obtener el valor ingresado
-      const numero = parseInt(numeroInput1.value);
-      const Element = document.createElement("h3");
-        if (esNumeroCuatroDigitos(numero)){
-            Element.textContent = encriptarEntero(numero);
-            const contenido1 = document.getElementById('contenido1');
-            const copiarBoton1 = document.getElementById('copiarBoton1')
+function desencriptarNumero(numero) {
+  // Convertir el número en una cadena de texto y asegurar que tenga 4 dígitos
+  let strNumero = numero.toString().padStart(4, '0');
 
-        }else{
-            Element.textContent = "ingrese numero entero de cuatro digitos";
+  // Obtener los dígitos individuales
+  let digito1 = parseInt(strNumero[0]);
+  let digito2 = parseInt(strNumero[1]);
+  let digito3 = parseInt(strNumero[2]);
+  let digito4 = parseInt(strNumero[3]);
 
-        }
-        // Agregar el elemento h1 al documento HTML
-        const contenidoDiv = document.getElementById("contenido1");
-        contenidoDiv.innerHTML = '';
-        contenidoDiv.appendChild(Element);
+  // Intercambiar los dígitos de nuevo al orden original
+  [digito1, digito3] = [digito3, digito1];
+  [digito2, digito4] = [digito4, digito2];
 
-    });
-    copiarBoton1.addEventListener('click', function() {
-        const textoEncriptado = contenido1.textContent;
-        copiarTextoAlPortapapeles(textoEncriptado);
-    });
+  // Desencriptar los dígitos
+  digito1 = (digito1 + 3) % 10;
+  digito2 = (digito2 + 3) % 10;
+  digito3 = (digito3 + 3) % 10;
+  digito4 = (digito4 + 3) % 10;
 
-    // Agregar un evento de escucha al campo de entrada2
-    numeroInput2.addEventListener('input', function() {
-        // Obtener el valor ingresado
-        const numero = parseInt(numeroInput2.value);
-        const Element = document.createElement("h3");
-        
-          if (esNumeroCuatroDigitos(numero)){
-              Element.textContent = desencriptarEntero(numero);
-          }else{
-              Element.textContent = "ingrese numero entero de cuatro digitos";
-          }
-          // Agregar el elemento h1 al documento HTML
-          const contenidoDiv = document.getElementById("contenido2");
-          contenidoDiv.innerHTML = '';
-          contenidoDiv.appendChild(Element);
-  
-      });
-  
-      copiarBoton2.addEventListener('click', function() {
-        const textoEncriptado = contenido2.textContent;
-        copiarTextoAlPortapapeles(textoEncriptado);
-    });
+  // Componer el número desencriptado y retornarlo
+  let numeroDesencriptado = parseInt(digito1.toString() + digito2.toString() + digito3.toString() + digito4.toString());
+  return numeroDesencriptado;
+}
+
+function esNumeroCuatroDigitos(numero) {
+  if (typeof numero === 'number' && Number.isInteger(numero) && numero >= 0 && numero <= 9999) {
+    return numero.toString().length <= 4;
+  } else {
+    return false;
+  }
+}
+
+function copiarTextoAlPortapapeles(texto) {
+  const elementoTemporal = document.createElement('textarea');
+  elementoTemporal.value = texto;
+  document.body.appendChild(elementoTemporal);
+  elementoTemporal.select();
+  document.execCommand('copy');
+  document.body.removeChild(elementoTemporal);
+}
+
+// Obtener referencia al campo de entrada y otros elementos
+const numeroInputEncriptar = document.getElementById('numeroInputEncriptar');
+const contenidoEncriptado = document.getElementById('contenidoEncriptado');
+const copiarBotonEncriptar = document.getElementById('copiarBotonEncriptar');
+const numeroInputDesencriptar = document.getElementById('numeroInputDesencriptar');
+const contenidoDesencriptado = document.getElementById('contenidoDesencriptado');
+const copiarBotonDesencriptar = document.getElementById('copiarBotonDesencriptar');
+
+// Agregar un evento de escucha al campo de entrada para encriptar
+numeroInputEncriptar.addEventListener('input', function() {
+  // Obtener el valor ingresado
+  const numero = parseInt(numeroInputEncriptar.value);
+  const resultado = document.createElement('h3');
+
+  if (esNumeroCuatroDigitos(numero)) {
+    resultado.textContent = encriptarNumero(numero);
+  } else {
+    resultado.textContent = 'Ingrese un número entero de cuatro dígitos';
+  }
+
+  contenidoEncriptado.innerHTML = '';
+  contenidoEncriptado.appendChild(resultado);
+});
+
+copiarBotonEncriptar.addEventListener('click', function() {
+  const textoEncriptado = contenidoEncriptado.textContent;
+  copiarTextoAlPortapapeles(textoEncriptado);
+});
+
+// Agregar un evento de escucha al campo de entrada para desencriptar
+numeroInputDesencriptar.addEventListener('input', function() {
+  // Obtener el valor ingresado
+  const numero = parseInt(numeroInputDesencriptar.value);
+  const resultado = document.createElement('h3');
+
+  if (esNumeroCuatroDigitos(numero)) {
+    resultado.textContent = desencriptarNumero(numero);
+  } else {
+    resultado.textContent = 'Ingrese un número entero de cuatro dígitos';
+  }
+
+  contenidoDesencriptado.innerHTML = '';
+  contenidoDesencriptado.appendChild(resultado);
+});
+
+copiarBotonDesencriptar.addEventListener('click', function() {
+  const textoDesencriptado = contenidoDesencriptado.textContent;
+  copiarTextoAlPortapapeles(textoDesencriptado);
+});
